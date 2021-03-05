682a0cc20da092691aa9a4b7fe6942e72962ce68
Alfonso Villanueva <avillanMty01@yahoo.com>
2021-03-04 18:28:09 -0600

    almost finished CSS

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@
 /* all credit to: Web Dev Simplyfied https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw*/
 import React, { useState } from 'react';
 import FlashcardList from './FlashcardList';
+import './App.css';
 
 const SAMPLE_FLASHCARDS = [
   {
     id: 1,
     question: 'What is 4+2 ',
-    answer: 4,
+    answer: 6,
     options: [ 2, 3, 5, 6]
   },
   {
     id: 2,
     question: 'Where is the Snoopy?',
-    answer: 4,
+    answer: 'baseball',
     options: [ 'garden', 'house', 'baseball']
   },
   {
     id: 3,
     question: 'Roberta___ toys are small.',
-    answer: 4,
+    answer: "'s",
     options: [ 's', "'s"]
   },
   {
     id: 4,
     question: 'The square root of 64: ',
-    answer: 4,
+    answer: 8,
     options: [ 32, 8, 16, 256]
   }
 ]
