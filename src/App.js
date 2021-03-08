b14d68a539e5efb20d5d03210bf70b18b9556326
Alfonso Villanueva <avillanMty01@yahoo.com>
2021-03-08 14:16:14 -0600

    End tutorial
    all working now

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
 /* an app that creates flashcards to prepare for a quiz */
 /* all credit to: Web Dev Simplyfied https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw*/
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from './FlashcardList';
 import './App.css';
 import axios from 'axios';
 
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: 'What is 4+2 ',
-    answer: 6,
-    options: [ 2, 3, 5, 6]
-  },
-  {
-    id: 2,
-    question: 'Where is the Snoopy?',
-    answer: 'baseball',
-    options: [ 'garden', 'house', 'baseball']
-  },
-  {
-    id: 3,
-    question: 'Roberta___ toys are small.',
-    answer: "'s",
-    options: [ 's', "'s"]
-  },
-  {
-    id: 4,
-    question: 'The square root of 64: ',
-    answer: 8,
-    options: [ 32, 8, 16, 256]
-  }
-]
-
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  const [flashcards, setFlashcards] = useState([])
+  const[categories, setCategories] = useState([])
+  const categoryEl= useRef()
+  const amountEl= useRef()
 
-  /* call axios to load questions and eecute as soon as component loads  using [] */
   useEffect(() => {
     axios
-      .get('https://opentdb.com/api.php?amount=10')
+      .get('https://opentdb.com/api_category.php')
+      .then(res => {
+        setCategories(res.data.trivia_categories)
+      })
+  }, [])
+
+  /* call axios to load questions and eecute as soon as component loads  using [] 
+  useEffect(() => {
+
+  }, [])*/
+
+  function decodeString(str) {
+    const textArea = document.createElement('textArea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios
+      .get('https://opentdb.com/api.php', {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value
+        }
+      })
       .then(res => {
         setFlashcards(res.data.results.map((questionItem, index) => {
           const answer = decodeString(questionItem.correct_answer)
@@ -54,17 +54,32 @@ function App() {
           }
         }))
       })
-  }, [])
-
-  function decodeString(str) {
-    const textArea = document.createElement('textArea')
-    textArea.innerHTML = str
-    return textArea.value
   }
+
   return (
-    <div className="container">
-      <FlashcardList flashcards={flashcards} />
-    </div>
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category: </label>
+          <select id="category" ref={categoryEl}>
+          {categories.map(category => {
+            return <option value={category.id} key={category.id}>{category.name}
+            </option>
+          })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number of questions: </label>
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref= {amountEl} />
+        </div>
+        <div className="form-group">
+          <button className="btn">Get questions</button>
+        </div>
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
   );
 }
 
